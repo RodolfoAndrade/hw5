1c5fb984a29e64c01230312216a6891fc52c9d31
Rodolfo Andrade <rodolfoivo@hotmail.com>
2022-11-29 13:00:06 +0100

    changing author in setup.py; adding test.py file

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages
 setup(
 	name="snowflake",
 	version="0.1",
-	authors="DSSS",
-	author_email="luisa.e.neubig@fau.de",
+	authors="Rodolfo Andrade",
+	author_email="rodolfo.i.santos@fau.de",
 	packages=find_packages(),
 	install_requires=["numpy", "turtles"],
 )
